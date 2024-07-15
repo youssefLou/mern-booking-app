5eae86cdcbf1461a74a7a9989bd0a09bb16669ce
youssefLou <louzani1999@gmail.com>
2024-07-15 20:45:53 +0100

    adde hotel feature and added e2e test

--- frontend/src/api-client.ts
+++ frontend/src/api-client.ts
@@ -84,3 +84,28 @@ export const validateToken = async () => {
     }
     return response.json()
   }
+
+  export const fetchMyHotelById= async (hotelId : string ): Promise<HotelType>=>{
+    const response = await fetch (`${API_BASE_URL}/api/my-hotels/${hotelId}`,{
+      credentials: "include"
+    })
+    if(!response.ok){
+      throw new Error ("Error fetching Hotels")
+    }
+
+    return response.json()
+  }
+
+  export const updateMyHotelById = async(hotelFormData : FormData)=>{
+    const response = await fetch (`${API_BASE_URL}/api/my-hotels/${hotelFormData.get("hotelId")}`,{
+      method:"PUT",
+      body : hotelFormData,
+      credentials:"include",
+    })
+    if(!response.ok){
+      throw new Error ("Failrd to update Hotel ")
+    }
+    return response.json()
+
+
+  }
---
+++ frontend/src/pages/EditHotel.tsx
@@ -0,0 +1,34 @@
+import { useMutation, useQuery } from "react-query";
+import { useParams } from "react-router-dom";
+import * as apiClient from "../api-client";
+import ManageHotelForm from "../forms/ManageHotelForm/ManageHotelForm";
+import { useContext } from "react";
+
+const EditHotel = () => {
+  const { hotelId } = useParams();
+  const { showToast } = useContext();
+  const { data: hotel } = useQuery(
+    "fetchMyHotelById",
+    () => apiClient.fetchMyHotelById(hotelId || ""),
+    {
+      enabled: !!hotelId,
+    }
+  );
+  const { mutate, isLoading } = useMutation(apiClient.updateMyHotelById, {
+    onSuccess: () => {
+      showToast({ message: "Hotel Saved! ", type: "SUCCESS" });
+    },
+    onError: () => {
+      showToast({ message: "Error Saveing Hotel", type: "ERROR" });
+    },
+  });
+
+  const handleSave = (hotelFormData: FormData) => {
+    mutate(hotelFormData);
+  };
+  return (
+    <ManageHotelForm hotel={hotel} onSave={handleSave} isLoading={isLoading} />
+  );
+};
+
+export default EditHotel;
